2371363204db8b19e987bda28ac10fcbbfc913ca
krystian-f <krystianfpa@gmail.com>
2022-07-18 14:05:05 +0200

    JS DOM variables

--- app.js
+++ app.js
@@ -1,2 +1,11 @@
 "use strict";
-console.log(`test`);
+
+// DOM
+const gameWinner = document.querySelector('.js-game__match');
+const roundWinner = document.querySelector('.js-game__round-winner');
+const roundHumanHand = document.querySelector('.js-game__round-human')
+const roundComputerHand = document.querySelector('.js-game__round-computer');
+const humanScore = document.querySelector('.js-player__score');
+const computerScore = document.querySelector('.js-computer__score');
+const humanHandChoose= document.querySelectorAll('.js-player__hand');
+const gameInstruction = document.querySelector('.js-game__instruction');
